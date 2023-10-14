209dc2d9c05bfd412f7c3372d3aa57fad6e02ebf
zxy <3077343086@qq.com>
2023-10-14 17:30:53 +0800

    2023/10/14

--- src/main/java/com/example/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.ArticleTagRelationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<ArticleTagRelationEntity, Long> {
-}
--- src/main/java/com/example/service/TagService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import java.util.List;
-
-public interface TagService {
-    List<String> listOfTags();
-}
